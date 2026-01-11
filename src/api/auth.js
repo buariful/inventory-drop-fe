ca9996fbe1497e02853c2edfe3e0ade67d15e3ad
ariful <arifulislam321054@gmail.com>
2026-01-11 21:18:37 +0600

    updated the url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,13 +1,13 @@
 import api, { saveTokens } from "./axios";
 
 export const loginUser = async (email, password) => {
-  const res = await api.post("/auth/login", { email, password });
+  const res = await api.post("/api/auth/login", { email, password });
   saveTokens(res.data.data.accessToken, res.data.data.refreshToken);
   return res.data.data; // contains user
 };
 
 export const signupUser = async ({ username, email, password }) => {
-  const res = await api.post("/auth/signup", { username, email, password });
+  const res = await api.post("/api/auth/signup", { username, email, password });
   saveTokens(res.data.data.accessToken, res.data.data.refreshToken);
   return res.data.data;
 };
@@ -15,7 +15,7 @@ export const logoutUser = async () => {
   const refreshToken = localStorage.getItem("refreshToken");
   if (refreshToken) {
     try {
-      await api.post("/auth/logout", { refreshToken });
+      await api.post("/api/auth/logout", { refreshToken });
     } catch (err) {
       console.error("Logout error:", err);
     }
